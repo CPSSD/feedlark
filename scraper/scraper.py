d1662925edf882825a3d52ba8c04aa5ef283c554
Cian Ruane <cian.ruane9@mail.dcu.ie>
2016-02-07 20:27:57 +0000

    Add requests and BeautifulSoup dependencies

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,13 +1,15 @@
 import sys
+import requests
 import feedparser
+from bs4 import BeautifulSoup
 
 class Scraper:
     def __init__(self):
         #Ensure the class is used with 3.5 or greater.
         assert sys.version_info >= (3,5)
 
-    def get_feed(self, url):
+    def get_feed_data(self, rss_url):
         pass
 
-    
-a = Scraper()
+    def _parse_from_web(self, article_url):
+        pass
