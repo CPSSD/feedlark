3a9e41f21a323bea21d3b82fa7991dd5915a660d
Cian Ruane <cian.ruane9@mail.dcu.ie>
2016-02-07 20:19:42 +0000

    Add feedparser requirement

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,10 +1,13 @@
-
+import sys
+import feedparser
 
 class Scraper:
-    def __init__():
-        pass
+    def __init__(self):
+        #Ensure the class is used with 3.5 or greater.
+        assert sys.version_info >= (3,5)
 
     def get_feed(self, url):
         pass
 
     
+a = Scraper()
