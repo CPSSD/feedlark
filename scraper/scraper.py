afaae860991543bc73bbf443e72cead1af0911c6
Cian Ruane <cian.ruane9@mail.dcu.ie>
2016-02-08 13:12:48 +0000

    Moved ross_scraper1 functionality to scraper class

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -9,7 +9,15 @@ class Scraper:
         assert sys.version_info >= (3,5)
 
     def get_feed_data(self, rss_url):
-        pass
+        feed = feedparser.parse(rss_url)
+        link_text = self._parse_from_web(feed["entries"][0]["links"][0]["href"])
+        return link_text
 
     def _parse_from_web(self, article_url):
-        pass
+        r = requests.get(article_url)
+        soup = BeautifulSoup(r.content,"html.parser")
+        return soup.get_text().encode('utf-8')
+
+#scr = Scraper()
+#print(scr.get_feed_data("http://matt.might.net/articles/feed.rss"))
+
