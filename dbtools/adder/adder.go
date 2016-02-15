c3df969954500fb93af83f57618ee3412fb70ff8
Noah Donnelly <iandioch11@gmail.com>
2016-02-15 04:45:57 +0000

    Add database update functions

--- dbtools/adder/adder.go
+++ dbtools/adder/adder.go
@@ -36,10 +36,25 @@ func AddDocument(dbUrl, database, collection, jsonData string) error {
 	return err
 }
 
+func UpdateDocument(dbUrl, database, collection, jsonData string) error {
+	coll := CreateSession(dbUrl, database, collection)
+	var data UpdateData
+	if err := json.Unmarshal([]byte(jsonData), &data); err != nil {
+		return err
+	}
+	err := coll.UpdateId(data.Id, data.Updates)
+	return err
+}
+
 type DbData struct {
 	Database   string `json:"database"`
 	Collection string `json:"collection"`
 	Data       string `json:"data"`
+}
+
+type UpdateData struct {
+	Updates string `json:"updates"`
+	Id      string `json:"id"`
 }
 
 func DbAdd(job worker.Job) ([]byte, error) {
@@ -52,6 +67,20 @@ func DbAdd(job worker.Job) ([]byte, error) {
 	if err != nil {
 		job.SendWarning([]byte("\"error\":\"" + err.Error() + "\""))
 		return nil, err
+	}
+	err = job.Err()
+	return nil, err
+}
+
+func DbUpdate(job worker.Job) ([]byte, error) {
+	fmt.Println("database updater called!")
+	var data DbData
+	if err := json.Unmarshal(job.Data(), &data); err != nil {
+		return nil, err
+	}
+	err := UpdateDocument("127.0.0.1:27017", data.Database, data.Collection, data.Data)
+	if err != nil {
+		job.SendWarning([]byte("\"error\":\"" + err.Error() + "\""))
 	}
 	err = job.Err()
 	return nil, err
