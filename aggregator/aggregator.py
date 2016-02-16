39fd6c370628f4322aec7e51a242b146b2b8a896
Cian Ruane <cian.ruane9@mail.dcu.ie>
2016-02-16 20:11:29 +0000

    Add get_user function

--- aggregator/aggregator.py
+++ aggregator/aggregator.py
@@ -13,7 +13,20 @@ def get_rss(gm_client):
     return json.loads(gm_job.result)
 
 def get_user(gm_client):
-    pass
+    request = json.dumps({
+        'database':'feedlark',
+        'collection':'users',
+        'query':''
+        'projection':{
+            'username':1,
+            'email':1,
+            'subscribed_feeds':1,
+            }
+        })
+
+    #submit_job as below is blocking
+    gm_job = gm_client.submit_job('db-get',request)
+    return json.loads(gm_job.result)
 
 def put_g2g(data):
     pass
