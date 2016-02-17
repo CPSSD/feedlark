819e84db9b604c793a60d632577511828e42323d
Aaron Delaney <devoxel@gmail.com>
2016-02-17 00:19:11 +0000

    Fix sails tests not passing due to Mongodb settings

--- server/test/bootstrap.test.js
+++ server/test/bootstrap.test.js
@@ -2,7 +2,7 @@ var sails = require('sails');
 
 before(function(done) {
   // Increase the Mocha timeout so that Sails has enough time to lift.
-  this.timeout(5000);
+  this.timeout(10000);
 
   sails.lift({
     // config
