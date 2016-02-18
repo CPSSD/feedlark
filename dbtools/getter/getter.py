3b42b0333c4baca437aa2642747e7e7db838f1f2
Noah Donnelly <iandioch11@gmail.com>
2016-02-18 20:05:33 +0000

    Modify db-get module to use BSON for gearman communication

--- dbtools/getter/getter.py
+++ dbtools/getter/getter.py
@@ -1,18 +1,21 @@
 import gearman
 import pymongo
-import json
+import bson # the pymongo bson lib
 
 print "Starting python worker 'db-get'"
 print "Initiating gearman worker"
-gearman_worker = gearman.GearmanWorker(["localhost:4730"])
+gearman_worker = gearman.GearmanWorker(["localhost:4731"])
 print "Initiating mongo client"
 mongo_client = pymongo.MongoClient('localhost', 27017)
 
 def task_listener_db_getter(worker, job):
-    print "Worker initiated"
     print "Worker initiated: "
-    data = json.loads(job.data)
-    print data
+    if not bson.is_valid(job.data):
+        print "Error: invalid bson"
+        return bson.BSON.encode({"status":"error", "error":"invalid bson"})
+    bsonObj = bson.BSON(job.data)
+    #data = bson.BSON.decode(bsonObj)
+    data = bsonObj.decode()
     db = data["database"]
     coll = data["collection"]
     query = data["query"]
@@ -21,19 +24,26 @@ def task_listener_db_getter(worker, job):
         projection = data["projection"]
     print "getting db"
     docs = mongo_client[db][coll].find(query, projection)
-    print str(docs)
-    results = "{\"docs\"=["
+    resultDocs = []
     index = 0
     for doc in docs:
-        print doc
-        if index > 0:
-            results += ","
-        results += str(doc)
-        index += 1
-    results += "]}"
-    print "results:"
+        resultDocs.append(doc)
+    results = {"docs":resultDocs}
+    print "results"
     print results
-    return results
+    resultBson = bson.BSON.encode(results)
+    print str(resultBson)
+    print bson.BSON.decode(resultBson)
+    return str(resultBson)
+
+class test_object:
+    def __init__(self):
+        self.data = bson.BSON.encode({"database":"feedlark", "collection":"feeds", "query":{}, "projection":{"_id":1, "url": 1}})
+#        self.data = '{"database":"feedlark"}'
+
+test_job = test_object()
+print test_job.data
+print task_listener_db_getter(None, test_job)
 
 print "Registering worker"
 gearman_worker.set_client_id("python-db-getter")
