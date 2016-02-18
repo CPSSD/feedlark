4fc6d76ed086b1cbd0b1abe83eb8f6f6fb87a8d5
Noah Donnelly <iandioch11@gmail.com>
2016-02-18 20:22:35 +0000

    Removed some fluff code in getter.py

--- dbtools/getter/getter.py
+++ dbtools/getter/getter.py
@@ -28,22 +28,10 @@ def task_listener_db_getter(worker, job):
     index = 0
     for doc in docs:
         resultDocs.append(doc)
-    results = {"docs":resultDocs}
-    print "results"
+    results = {"docs":resultDocs, "status":"ok"}
     print results
     resultBson = bson.BSON.encode(results)
-    print str(resultBson)
-    print bson.BSON.decode(resultBson)
     return str(resultBson)
-
-class test_object:
-    def __init__(self):
-        self.data = bson.BSON.encode({"database":"feedlark", "collection":"feeds", "query":{}, "projection":{"_id":1, "url": 1}})
-#        self.data = '{"database":"feedlark"}'
-
-test_job = test_object()
-print test_job.data
-print task_listener_db_getter(None, test_job)
 
 print "Registering worker"
 gearman_worker.set_client_id("python-db-getter")
