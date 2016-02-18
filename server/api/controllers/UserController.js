db4b864692459809b0ee4795575f6c03795be73e
Aaron Delaney <devoxel@gmail.com>
2016-02-18 02:55:47 +0000

    Add basic functiosn to User API

--- server/api/controllers/UserController.js
+++ server/api/controllers/UserController.js
@@ -6,6 +6,51 @@
  */
 
 module.exports = {
-	
-};
 
+
+
+  /**
+   * `UserController.login()`
+   */
+  login: function (req, res) {
+		return res.login({
+			username: req.param('username'),
+      email: req.param('email'),
+      password: req.param('password'),
+      successRedirect: '/',
+      invalidRedirect: '/login'
+    });
+  },
+
+
+  /**
+   * `UserController.logout()`
+   */
+  logout: function (req, res) {
+    req.session.me = null;
+		if (req.wantsJSON) {
+      return res.ok('Logged out! See you next time :)');
+    }
+		return res.redirect('/');
+  },
+
+
+  /**
+   * `UserController.singup()`
+   */
+  singup: function (req, res) {
+		User.signup({
+      name: req.param('username'),
+      email: req.param('email'),
+      password: req.param('password')
+    }, function (err, user) {
+      if (err) return res.negotiate(err);
+
+      req.session.me = user.id;
+      if (req.wantsJSON) {
+        return res.ok('Signup successful! Horray!');
+      }
+      return res.redirect('/welcome');
+    });
+  }
+};
--- server/api/models/User.js
+++ server/api/models/User.js
@@ -1,18 +1,21 @@
 /**
  * User.js
  *
- * @description :: Here is the fundementially important user model. It describes
- *                 what the server thinks a user is.
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
 module.exports = {
+
   attributes: {
+
     username: {
-      type: 'string'
+      type: 'string',
+      required: true
     },
     email: {
       type: 'string',
+      required: true,
       unique: true
     },
     hashed_password: {
@@ -23,6 +26,27 @@ module.exports = {
     },
     subscribed_feeds: {
       type: 'array'
+    },
+
+    signup: function (inputs, cb) {
+      // Create a user
+      User.create({
+        name: inputs.name,
+        email: inputs.email,
+        // TODO: But encrypt the password first
+        password: inputs.password
+      })
+        .exec(cb);
+    },
+    attemptLogin: function (inputs, cb) {
+      // Create a user
+      User.findOne({
+        email: inputs.email,
+        // TODO: But encrypt the password first
+        password: inputs.password
+      })
+      .exec(cb);
     }
+
   }
 };
