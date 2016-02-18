488275efc84128ad72b1e399b3e7e1e8710b51d9
Noah Donnelly <iandioch11@gmail.com>
2016-02-18 21:38:28 +0000

    Update db-add and db-update to use BSON over gearman

--- dbtools/adder/adder.go
+++ dbtools/adder/adder.go
@@ -1,10 +1,10 @@
 package adder
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/mikespook/gearman-go/worker"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func Create() error {
@@ -14,14 +14,16 @@ func Create() error {
 		fmt.Println(e)
 	}
 	w.AddServer("tcp4", "127.0.0.1:4730")
+	fmt.Println("Adding 'db-add' function.")
 	w.AddFunc("db-add", DbAdd, worker.Unlimited)
+	fmt.Println("Adding 'db-update' function.")
 	w.AddFunc("db-update", DbUpdate, worker.Unlimited)
 	if err := w.Ready(); err != nil {
 		fmt.Println("Fatal error")
 		fmt.Println(err)
 		return err
 	}
-	go w.Work()
+	w.Work()
 	return nil
 }
 
@@ -30,58 +32,79 @@ func CreateSession(url string, database string, collection string) *mgo.Collecti
 	return session.DB(database).C(collection)
 }
 
-func AddDocument(dbUrl, database, collection, jsonData string) error {
+func AddDocument(dbUrl, database, collection string, jsonData bson.M) ([]byte, error) {
+	fmt.Println("Adding document to db " + database + " collection " + collection)
+	id, alreadyHasId := jsonData["_id"]
+	if !alreadyHasId {
+		id = bson.NewObjectId()
+		jsonData["_id"] = id
+	}
 	coll := CreateSession(dbUrl, database, collection)
 	err := coll.Insert(jsonData)
-	return err
+	if err != nil {
+		fmt.Println(err)
+	}
+	response := bson.M{"status": "ok", "_id": id}
+	bson, _ := bson.Marshal(response)
+	return bson, err
 }
 
-func UpdateDocument(dbUrl, database, collection, jsonData string) error {
+func UpdateDocument(dbUrl, database, collection string, jsonData bson.M) ([]byte, error) {
+	fmt.Println("Updating document in db " + database + " collection " + collection)
 	coll := CreateSession(dbUrl, database, collection)
-	var data UpdateData
-	if err := json.Unmarshal([]byte(jsonData), &data); err != nil {
-		return err
+	err := coll.Update(jsonData["selector"], jsonData["updates"])
+	if err != nil {
+		fmt.Println(err)
 	}
-	err := coll.UpdateId(data.Id, data.Updates)
-	return err
+	response := bson.M{"status": "ok"}
+	bson, _ := bson.Marshal(response)
+	return bson, err
 }
 
 type DbData struct {
-	Database   string `json:"database"`
-	Collection string `json:"collection"`
-	Data       string `json:"data"`
+	Database   string `bson:"database"`
+	Collection string `bson:"collection"`
+	Data       bson.M `bson:"data"`
 }
 
 type UpdateData struct {
-	Updates string `json:"updates"`
-	Id      string `json:"id"`
+	Updates string `bson:"updates"`
+	Id      string `bson:"_id"`
 }
 
 func DbAdd(job worker.Job) ([]byte, error) {
 	fmt.Println("database adder called!")
+	fmt.Println(string(job.Data()))
 	var data DbData
-	if err := json.Unmarshal(job.Data(), &data); err != nil {
+	if err := bson.Unmarshal(job.Data(), &data); err != nil {
+		fmt.Println(err)
 		return nil, err
 	}
-	err := AddDocument("127.0.0.1:27017", data.Database, data.Collection, data.Data)
+	fmt.Println(data)
+	fmt.Println("Trying to add document to db")
+	response, err := AddDocument("127.0.0.1:27017", data.Database, data.Collection, data.Data)
 	if err != nil {
+		fmt.Println(err)
 		job.SendWarning([]byte("\"error\":\"" + err.Error() + "\""))
 		return nil, err
 	}
 	err = job.Err()
-	return nil, err
+	return response, err
 }
 
 func DbUpdate(job worker.Job) ([]byte, error) {
 	fmt.Println("database updater called!")
 	var data DbData
-	if err := json.Unmarshal(job.Data(), &data); err != nil {
+	if err := bson.Unmarshal(job.Data(), &data); err != nil {
+		fmt.Println(err)
 		return nil, err
 	}
-	err := UpdateDocument("127.0.0.1:27017", data.Database, data.Collection, data.Data)
+	fmt.Println("DbUpdate data: ", data)
+	response, err := UpdateDocument("127.0.0.1:27017", data.Database, data.Collection, data.Data)
 	if err != nil {
+		fmt.Println(err)
 		job.SendWarning([]byte("\"error\":\"" + err.Error() + "\""))
 	}
 	err = job.Err()
-	return nil, err
+	return response, err
 }
