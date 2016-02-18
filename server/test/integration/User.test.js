c048a7a3986344b6b053171d0b2da0832ddc1934
Aaron Delaney <devoxel@gmail.com>
2016-02-18 05:15:58 +0000

    ÂAdd login test

--- server/test/integration/User.test.js
+++ server/test/integration/User.test.js
@@ -3,19 +3,39 @@ var assert = require('assert');
 var request = require('supertest');
 
 describe('UserModel', function() {
+  var user_details = {
+    username: "rms",
+    email: "rms@gnu.org",
+    entry_password: "gnuisnotlinux"
+  };
   describe('#signup()', function() {
-    it('should check signup function', function (done) {
-      var user_details = {
-        username: "rms",
-        email: "rms@gnu.org",
-        entry_password: "gnuisnotlinux"
-      };
+    it('should check signup functionality', function (done) {
       request(sails.hooks.http.app)
         .post('/user/signup')
         .type('form')
         .send(user_details)
         .expect(200)
         .expect("Signup successful! Horray!");
+      done();
+    });
+  });
+  describe('#signup()', function() {
+    it('should check users cant signup twice', function (done) {
+      request(sails.hooks.http.app)
+        .post('/user/signup')
+        .type('form')
+        .send(user_details)
+        .expect(400);
+      done();
+    });
+  });
+  describe('#login()', function() {
+    it('should check login in UserController', function (done) {
+      request(sails.hooks.http.app)
+        .post('/user/login')
+        .type('form')
+        .send(user_details)
+        .expect(200);
       done();
     });
   });
