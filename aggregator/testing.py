bae3e298d93c366efe60e860adf90b4645cd54d4
Cian Ruane <cian.ruane9@mail.dcu.ie>
2016-02-20 01:15:30 +0000

    Add put_g2g testing

--- aggregator/testing.py
+++ aggregator/testing.py
@@ -52,7 +52,39 @@ class TestAggregation(unittest.TestCase):
                 u"arictle_text": u"Building the Tamagotchi Singularity. The Singularity has happened, but not to us. I also gave a talk about this project on the Hackaday Superconference 2015. There is a video available of that if you'd rather watch me talk. You can also directly view the end result if you so please. As some of you may know, I recently moved from the Netherlands to Shanghai. In the long term, this is great for my hobby: I got a fair amount of stuff out of China anyway, and me moving there meant I wouldn't have to wait a month for it to arrive anymore. In the short term, my ability to build things took somewhat of a hit, though: aside from my oscilloscope and some small bits and bobs I thought I would have a hard time getting in China, I left most of my electronic stuff back in the Netherlands, with the idea that I would be able to buy myself most stuff anew when I had the time."
             }]
         self.assertEqual(feed_items[0], expected_response[0])
-        
+
+    def test_put_g2g(self):
+        gm_client = gearman.GearmanClient(['localhost:4730'])
+        agg = Aggregator(gm_client)
+
+##        add_request = bson.BSON.encode({
+##            'database':'feedlark',
+##            'collecion':'g2g',
+##            'data':{
+##                'username':'__test123__',
+##                'test_parameter':'NOLO'
+##                }
+##            })
+##        gm_client.submit_job('db-add',str(add_request))
+
+        test_document = {
+            'username':'iandioch',
+            'test_parameter':'YOLO',
+            }
+        agg.put_g2g('iandioch',test_document)
+
+        get_request = bson.BSON.encode({
+            'database':'feedlark',
+            'collection':'g2g',
+            'query':{
+                'username':'iandioch',
+                },
+            'projection':{
+                'test_parameter':1,
+                },
+            })
+        g2g_data = gm_client.submit_job('db-get',str(get_request)).result
+        self.assertEqual(bson.BSON(g2g_data).decode()['docs'][0]['test_parameter'], 'YOLO')
         
     
 
