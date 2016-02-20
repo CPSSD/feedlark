054fd084532df13cd33b5b411a8738a276a84250
SailSlick <ross.osullivan37@mail.dcu.ie>
2016-02-20 10:27:51 +0000

    Fixed indents in gman_scraper

--- scraper/gman_scraper.py
+++ scraper/gman_scraper.py
@@ -13,14 +13,14 @@ def get_all_feed_ids_url():
 	to_get_urls_ids = str(bson.BSON.encode({"database":"feedlark","collection":"feed", "query": {},"projection":{"_id":1, "url":1}}))
 	url_fields_gotten = gm_client.submit_job("db-get", to_get_urls_ids)
 	bson_object = bson.BSON.decode(bson.BSON(url_fields_gotten.result))
-        print "response: " + str(bson_object)
+	print "response: " + str(bson_object)
 
 	#extract the url and id strings
 	urls = []
 	ids = []
 	for item in bson_object["docs"]:
-                if "url" not in item or "_id" not in item:
-                    continue
+		if "url" not in item or "_id" not in item:
+			continue
 		urls.append(str(item['url']))
 		ids.append(item['_id'])
 
@@ -31,29 +31,29 @@ def get_all_feed_ids_url():
 def update_all_feeds(worker,job):
 	print "'update-all-feeds' initiated"
 	feed_urls, feed_ids = get_all_feed_ids_url()
-        print feed_urls
+	print feed_urls
 	print "Retrieving data from feed db"
 
 	for i in range(len(feed_urls)):
 		print "Loading items in feed " + str(feed_urls[i])
-                result = scr.get_feed_data(feed_urls[i]) # returns a list of dictionaries, a dict for each item in the feed
-                bson_data = None
-                try:
-                    bson_data = bsonify_update_data(feed_ids[i], feed_urls[i], result)
-                except Exception as e:
-                    print e
-                    return str(bson.BSON.encode({"status":"error", "error-description":str(e)}))
-                print "ready to db-update"
-                update_response = None
-                try:
-                    update_response = gm_client.submit_job('db-update', str(bson_data), background=True)
-                except Exception as e:
-                    print e
-                    return str(bson.BSON.encode({"status":"error", "error-description":str(e)}))
-                print "update response: " + str(update_response)
+		result = scr.get_feed_data(feed_urls[i]) # returns a list of dictionaries, a dict for each item in the feed
+		bson_data = None
+		try:
+			bson_data = bsonify_update_data(feed_ids[i], feed_urls[i], result)
+		except Exception as e:
+			print e
+			return str(bson.BSON.encode({"status":"error", "error-description":str(e)}))
+		print "ready to db-update"
+		update_response = None
+		try:
+			update_response = gm_client.submit_job('db-update', str(bson_data), background=True)
+		except Exception as e:
+			print e
+			return str(bson.BSON.encode({"status":"error", "error-description":str(e)}))
+		print "update response: " + str(update_response)
 
 	print "'update-all-feeds' finished"
-        return str(bson.BSON.encode({"updated_feeds":feed_ids, "status":"ok"}))
+	return str(bson.BSON.encode({"updated_feeds":feed_ids, "status":"ok"}))
 
 
 scr = Scraper()
@@ -68,5 +68,4 @@ gm_client = gearman.GearmanClient(["localhost:4730"])
 # register the tasks -> update all the feeds, add new feeds.
 print "Registering task 'update-all-feeds'"
 gm_worker.register_task('update-all-feeds', update_all_feeds)
-#gm_worker.register_task('add-new-feed', feed_url_to_add)
 gm_worker.work()
