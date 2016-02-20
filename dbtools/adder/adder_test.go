4644ba243a8935ddb0e8ccae83112b87e93239d7
Noah Donnelly <iandioch11@gmail.com>
2016-02-20 19:27:35 +0000

    Add unit tests

--- dbtools/adder/adder_test.go
+++ dbtools/adder/adder_test.go
@@ -1,7 +1,9 @@
 package adder
 
 import (
+	"fmt"
 	"gopkg.in/mgo.v2/bson"
+	//"strconv"
 	"testing"
 )
 
@@ -21,5 +23,46 @@ func TestDocumentUpdate(t *testing.T) {
 	_, err = UpdateDocument("127.0.0.1:27017", "testing", "updater", bson.M{"selector": bson.M{"_id": id}, "updates": bson.M{"memes": "are ok"}})
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestNewDocumentUpsert(t *testing.T) {
+	rawResult, err := UpsertDocument("127.0.0.1:27017", "testing", "upserter", bson.M{"selector": bson.M{"memes": "are not ok"}, "updates": bson.M{"memes": "are ok"}})
+	if err != nil {
+		t.Error(err)
+	}
+	var result struct {
+		Status string
+		NewDoc bool `bson:"new_doc"`
+	}
+	bson.Unmarshal(rawResult, &result)
+	if result.Status != "ok" {
+		fmt.Println("Status != ok")
+		t.FailNow()
+	}
+	if !result.NewDoc {
+		fmt.Println("Is updating old doc, should be new doc")
+		t.FailNow()
+	}
+}
+
+func TestExistingDocumentUpsert(t *testing.T) {
+	AddDocument("127.0.0.1:27017", "testing", "upserter", bson.M{"love": "memes"})
+	rawResult, err := UpsertDocument("127.0.0.1:27017", "testing", "upserter", bson.M{"selector": bson.M{"love": "memes"}, "updates": bson.M{"love": "dank memes"}})
+	if err != nil {
+		t.Error(err)
+	}
+	var result struct {
+		Status string
+		NewDoc bool `bson:"new_doc"`
+	}
+	bson.Unmarshal(rawResult, &result)
+	if result.Status != "ok" {
+		fmt.Println("Status != ok")
+		t.FailNow()
+	}
+	if result.NewDoc {
+		fmt.Println("Is new doc, should be old")
+		t.FailNow()
 	}
 }
