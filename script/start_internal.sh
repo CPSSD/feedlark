11f30c96e1b40d8de97627426ff472685958a81f
Lucas Savva <lucas@vccomputers.ie>
2016-02-21 20:34:14 +0000

    Log screen sessions. Add stop scripts

--- script/start_internal.sh
+++ script/start_internal.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 echo "Starting backend elements in screen sessions"
 cd /vagrant
-screen -dmS backend-getter python dbtools/getter/getter.py
-screen -dmS backend-workers go run dbtools/start_workers.go
-screen -dmS backend-aggregator python aggregator/aggregator.py
-screen -dmS backend-scraper python scraper/gman_scraper.py
-screen -dmS backend-scheduler python scheduler/scheduler.py
+mkdir -p log/backend-getter
+cd log/backend-getter
+screen -dmLS backend-getter python /vagrant/dbtools/getter/getter.py
+cd ..
+mkdir -p backend-workers
+cd backend-workers
+screen -dmLS backend-workers go run /vagrant/dbtools/start_workers.go
+cd ..
+mkdir -p backend-aggregator
+cd backend-aggregator
+screen -dmLS backend-aggregator python /vagrant/aggregator/aggregator.py
+cd ..
+mkdir -p backend-scraper
+cd backend-scraper
+screen -dmLS backend-scraper python /vagrant/scraper/gman_scraper.py
+cd ..
+mkdir -p backend-scheduler
+cd backend-scheduler
+screen -dmLS backend-scheduler python /vagrant/scheduler/scheduler.py
 echo "Starting frontend elements in screen sessions"
 cd /vagrant/server
-screen -dmS frontend-sails /usr/bin/npm run start
+screen -dmLS frontend-sails /usr/bin/npm run start
 echo "Running! Please allow ~20 seconds for everything to initialise"
 echo "Running screens (Should be 6):"
 sleep 1
 screen -list
-
