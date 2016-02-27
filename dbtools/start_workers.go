ff4b0207a2188a5b018a1313c2ade735b5f91c1f
Noah Donnelly <iandioch11@gmail.com>
2016-02-27 23:57:55 +0000

    Switch from Python db-get to Go version

--- dbtools/start_workers.go
+++ dbtools/start_workers.go
@@ -10,14 +10,4 @@ func main() {
 	fmt.Println("Starting dbtools Gearman workers.")
 	go adder.Create()
 	go getter.Create()
-	err := adder.Create()
-	if err != nil {
-		fmt.Println("Could not create Adder worker: ")
-		fmt.Println(err)
-	}
-	err = getter.Create()
-	if err != nil {
-		fmt.Println("Could not create Getter worker: ")
-		fmt.Println(err)
-	}
 }
