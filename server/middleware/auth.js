5824a7fdbebd465472a18331dde0ea1dcd57d906
Aaron Delaney <devoxel@gmail.com>
2016-03-01 15:49:12 +0000

    Add auth.js and add jslint

---
+++ server/middleware/auth.js
@@ -0,0 +1,24 @@
+/*
+# Auth.js
+
+Some authorization helper functions
+*/
+
+export function isAuthed(req, res, next) {
+  if (req.session.username) {
+    next(req, res);
+  }
+  else {
+    res.status(403).end();
+  }
+}
+
+export function authorise (req, res, user, next) {
+  req.session.username = user;
+  next(req, res);
+}
+
+export function deauthorize (req, res, next) {
+  req.session.destroy();
+  next(req, res);
+}
