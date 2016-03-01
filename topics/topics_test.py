743b83e38140596f87ea4aa56536de75bc6a1f01
Noah Donnelly <iandioch11@gmail.com>
2016-03-01 18:29:05 +0000

    Fix unit tests

--- topics/topics_test.py
+++ topics/topics_test.py
@@ -5,27 +5,27 @@ class TestTopics(unittest.TestCase):
     def test_topics_without_stop_words(self):
         article = "banana banana banana"
         topics = get_topics(article)
-        self.assertEquals(len(topics), 1)
-        self.assertEquals(topics[0], "banana")
+        self.assertEqual(len(topics), 1)
+        self.assertTrue("banana" in topics)
 
     def test_topics_with_stop_words(self):
         article = "banana banana banana the"
         topics = get_topics(article)
-        self.assertEquals(len(topics), 1)
-        self.assertEquals(topics[0], "banana")
+        self.assertEqual(len(topics), 1)
+        self.assertTrue("banana" in topics)
         self.assertFalse("the" in topics)
 
     def test_multiple_topics_without_stop_words(self):
         article = "banana banana banana orange orange"
         topics = get_topics(article)
-        self.assertEquals(len(topics), 2)
+        self.assertEqual(len(topics), 2)
         self.assertTrue("banana" in topics)
         self.assertTrue("orange" in topics)
 
     def test_multiple_topics_with_stop_words(self):
         article = "the banana a banana an banana or orange and orange"
         topics = get_topics(article)
-        self.assertEquals(len(topics), 2)
+        self.assertEqual(len(topics), 2)
         self.assertTrue("banana" in topics)
         self.assertTrue("orange" in topics)
         self.assertFalse("the" in topics)
