ec6bdf95dbbc808403fa3a50474737f335bdca47
Noah Donnelly <iandioch11@gmail.com>
2016-03-03 23:21:12 +0000

    Make topics work in Python 2

--- topics/topics.py
+++ topics/topics.py
@@ -1,7 +1,9 @@
 import re
+import gearman, bson
 from spacy.en import English
 
 nlp = English()
+gearman_client = None
 
 def remove_stop_words(doc):
     new_doc = []
@@ -29,3 +31,19 @@ def get_topics(article):
     word_counts = count_words(doc)
     print(word_counts)
     return word_counts
+
+def get_topics_gearman(job_data):
+    data = bson.BSON.decode(job_data)
+
+    print(data)
+
+    response = {"status":"ok"}
+    bson_response = bson.BSON.encode(response)
+    return bson_response
+
+if __name__ == '__main__':
+    gearman_client = gearman.GearmanClient(['localhost:4730'])
+    gearman_worker = gearman.GearmanWorker(['localhost:4730'])
+    gearman_worker.set_client_id('topic-modeller')
+    gearman_worker.register_task('get-topics', get_topics_gearman)
+    gearman_worker.work()
--- topics/topics_test.py
+++ topics/topics_test.py
@@ -3,27 +3,27 @@ from topics import get_topics
 
 class TestTopics(unittest.TestCase):
     def test_topics_without_stop_words(self):
-        article = "banana banana banana"
+        article = u"banana banana banana"
         topics = get_topics(article)
         self.assertEqual(len(topics), 1)
         self.assertTrue("banana" in topics)
 
     def test_topics_with_stop_words(self):
-        article = "banana banana banana the"
+        article = u"banana banana banana the"
         topics = get_topics(article)
         self.assertEqual(len(topics), 1)
         self.assertTrue("banana" in topics)
         self.assertFalse("the" in topics)
 
     def test_multiple_topics_without_stop_words(self):
-        article = "banana banana banana orange orange"
+        article = u"banana banana banana orange orange"
         topics = get_topics(article)
         self.assertEqual(len(topics), 2)
         self.assertTrue("banana" in topics)
         self.assertTrue("orange" in topics)
 
     def test_multiple_topics_with_stop_words(self):
-        article = "the banana a banana an banana or orange and orange"
+        article = u"the banana a banana an banana or orange and orange"
         topics = get_topics(article)
         self.assertEqual(len(topics), 2)
         self.assertTrue("banana" in topics)
