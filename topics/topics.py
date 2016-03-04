84a20298e5a4520fd2796f0cc615fb3d6dc2bda1
Noah Donnelly <iandioch11@gmail.com>
2016-03-04 01:51:16 +0000

    Modify topics.py to adhere to logging spec£

--- topics/topics.py
+++ topics/topics.py
@@ -1,9 +1,15 @@
 import re
+from datetime import datetime
 import gearman, bson
 from spacy.en import English
 
 nlp = English()
 gearman_client = None
+
+def log(level, message):
+    time = datetime.now().strftime('%H:%M %d/%m/%Y')
+    levels = ['INFO', 'WARNING', 'ERROR']
+    print(str(time) + " " + levels[level] + ": " + str(message))
 
 def remove_stop_words(doc):
     new_doc = []
@@ -15,7 +21,7 @@ def remove_stop_words(doc):
 def count_words(word_list):
     words = {}
     total = 0
-    print("Word list: " + str(word_list))
+    log(0, "Word list: " + str(word_list))
     for orig_word in word_list:
         word = orig_word.text.lower().strip()
         if word in words:
@@ -27,38 +33,32 @@ def count_words(word_list):
 
 def get_topics(article):
     doc = nlp(article)
-    print("orig: " + str(doc))
+    log(0, "orig: " + str(doc))
     doc = remove_stop_words(doc)
-    print("sans stop words: " + str(doc))
+    log(0, "sans stop words: " + str(doc))
     word_counts, total = count_words(doc)
-    print(word_counts)
+    log(0, word_counts)
     normalised_counts = {k:(v/float(total)) for k,v in word_counts.items()}
     return normalised_counts
 
 def get_topics_gearman(worker, job):
-    print("Get topics: " + job.data)
+    log(0, "Get topics: " + job.data)
     bson_obj = bson.BSON(job.data)
     data = bson_obj.decode() 
     if "article" not in data:
         return str(bson.BSON.encode({"status":"error", "description":"No article supplied"}))
-    print("Article: " + data["article"])
+    log(0, "Article: " + data["article"])
 
     article = data["article"]
     topics = get_topics(article)
     if(len(topics) > 10):
-        try:
-            print("More than 10 topics")
-            sorted_topics = sorted(topics.items(), key=lambda x : x[1], reverse=True)
-            print(sorted_topics)
-            new_topics = {}
-            for i in xrange(10):
-                new_topics[sorted_topics[i][0]] = sorted_topics[i][1]
-            topics = new_topics
-        except Exception as e:
-
-            print(e)
-
-    print(topics)
+        log(0, "More than 10 topic words, returning only most frequent 110")
+        sorted_topics = sorted(topics.items(), key=lambda x : x[1], reverse=True)
+        log(0, sorted_topics)
+        new_topics = {}
+        for i in xrange(10):
+            new_topics[sorted_topics[i][0]] = sorted_topics[i][1]
+        topics = new_topics
 
     response = {"status":"ok", "topics":topics}
     bson_response = bson.BSON.encode(response)
@@ -68,6 +68,6 @@ if __name__ == '__main__':
     gearman_client = gearman.GearmanClient(['localhost:4730'])
     gearman_worker = gearman.GearmanWorker(['localhost:4730'])
     gearman_worker.set_client_id('topic-modeller')
-    print("Registering gearman worker 'get-topics'")
+    log(0, "Registering gearman worker 'get-topics'")
     gearman_worker.register_task('get-topics', get_topics_gearman)
     gearman_worker.work()
