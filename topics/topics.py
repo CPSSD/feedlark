d63540c76475690695c52904baa85ac1ca800a90
iandioch <iandioch11@gmail.com>
2016-03-10 23:24:55 +0000

    Remove overzealous logging

--- topics/topics.py
+++ topics/topics.py
@@ -27,7 +27,6 @@ def limit_dict(d, num):
     if len(d) <= num:
         return d
     sorted_d = sorted(d.items(), key=lambda x : x[1], reverse=True)
-    log(0, sorted_d)
     e = {}
     for i in xrange(num):
         e[sorted_d[i][0]] = sorted_d[i][1]
@@ -44,7 +43,6 @@ def remove_stop_words(doc):
 def count_words(word_list):
     words = {}
     total = 0
-    log(0, "Word list: " + str(word_list))
     for orig_word in word_list:
         word = orig_word.text.lower().strip()
         if word in words:
@@ -56,21 +54,16 @@ def count_words(word_list):
 
 def get_topics(article):
     doc = nlp(article)
-    log(0, "orig: " + str(doc))
     doc = remove_stop_words(doc)
-    log(0, "sans stop words: " + str(doc))
     word_counts, total = count_words(doc)
-    log(0, word_counts)
     normalised_counts = {k:(v/float(total)) for k,v in word_counts.items()}
     return normalised_counts
 
 def get_topics_gearman(worker, job):
-    log(0, "Get topics: " + job.data)
     bson_obj = bson.BSON(job.data)
     data = bson_obj.decode() 
     if "article" not in data:
         return str(bson.BSON.encode({"status":"error", "description":"No article supplied"}))
-    log(0, "Article: " + data["article"])
 
     article = data["article"]
     topics = get_topics(article)
@@ -82,16 +75,13 @@ def get_topics_gearman(worker, job):
         log(0, 'got topics, sending to db')
         gclient = gearman.GearmanClient(['localhost:4730'])
         log(0, '_id:' + str(bson.ObjectId(data['_id'])))
-        log(0, data)
         db_resp = bson.BSON.decode(bson.BSON(gclient.submit_job('db-get', str(bson.BSON.encode({'database':'feedlark', 'collection':'feed', 'query':{u'_id':data[u'_id']}, 'projection':{'_id':1, 'items':1}}))).result))
         if len(db_resp['docs']) == 0:
              raise Exception('No documents returned with given query.')
         old_data = db_resp['docs'][0]
-        log(0, 'old_data: ' + str(old_data))
         modifications = {"topics":topics}
         link = data['link']
         new_data = update_article_data(old_data, link, modifications)
-        log(0, 'new_data: ' + str(new_data))
         r = bson.BSON.decode(bson.BSON(gclient.submit_job('db-update', str(bson.BSON.encode({'database':'feedlark', 'collection': 'feed', 'data':{'selector':{'_id':data['_id']}, 'updates':new_data}}))).result))
     except Exception as e:
 	log(2, str(e))
