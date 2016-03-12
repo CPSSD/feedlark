e848a39e96e44d7a8121915a6744a92473b07ad5
m1cr0man <lucas@vccomputers.ie>
2016-03-12 16:50:54 +0000

    Add selection of items per page

--- server/controllers/streams.js
+++ server/controllers/streams.js
@@ -7,21 +7,21 @@
 
 const getFeeds = require("../models/stream").getFeeds;
 const _ = require("lodash");
-const page_length = 20;
 
 // Stream listing
 module.exports = {
   index: (req, res) => {
 
-    // Get & verify the page number
+    // Get & verify the page length & number
     // This Lo-Dash function is lovely
+    var page_length = _.toSafeInteger(req.params.page_length);
     var page = _.toSafeInteger(req.params.page);
 
     getFeeds(req.session.username, feeds => {
 
       // Get a page worth of feeds
       var pageinated_feeds = [];
-      for (var i = page * page_length; i <= (page + 1) * page_length && i < feeds.length; i++) {
+      for (var i = page * page_length; i < (page + 1) * page_length && i < feeds.length; i++) {
         pageinated_feeds.push(feeds[i]);
       }
 
@@ -29,7 +29,7 @@ module.exports = {
       var next_page = page + 1;
       if ((page + 1) * page_length > feeds.length) next_page = 0;
 
-      res.status(200).render("stream_index", {feeds: pageinated_feeds, page: next_page});
+      res.status(200).render("stream_index", {feeds: pageinated_feeds, page: next_page, page_length: page_length});
     });
   }
 };
