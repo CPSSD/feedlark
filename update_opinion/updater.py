08145ff401000ee988c464b7f4785aaadb891857
iandioch <iandioch11@gmail.com>
2016-03-14 00:58:05 +0000

    Make updater log article votes to db

--- update_opinion/updater.py
+++ update_opinion/updater.py
@@ -2,6 +2,8 @@ import gearman
 import bson
 from datetime import datetime
 
+gearman_client = None
+
 def log(level, message):
     levels = ['INFO:', 'WARNING:', 'ERROR:']
     time = datetime.now().strftime('%H:%M %d/%m/%Y')
@@ -16,11 +18,24 @@ def update_topic_counts(old_topics, changes, is_positive):
             old_topics[change] = diff
     return old_topics
 
-gearman_client = None
+def add_update_to_db(data):
+    req_data = {"database":"feedlark", "collection":"votes", "data":data}
+    bson_req = bson.BSON.encode(req_data)
+    gearman_client.submit_job('db-add', str(bson_req))
 
 def update_user_model(worker, job):
     bson_input = bson.BSON(job.data)
     input = bson_input.decode()
+    add_update_to_db(input)
+    log(0, 'update-user-model called with data ' + str(input))
+    if not ("username" in input and "feed_url" in input and "article_url" in input):
+        log(1, 'Missing field in input: ' + str(input))
+        response = {"status":"error", "description":"Missing field in input."}
+        bson_response = bson.BSON.encode(response)
+        return str(bson_response)
+    response = {"status":"ok"}
+    bson_response = bson.BSON.encode(response)
+    return str(bson_response)
 
 if __name__ == '__main__':
     log(0, "Creating gearman client.")
