7286015831e4e3261d990c7b9babaee14ff59f65
m1cr0man <lucas@vccomputers.ie>
2016-03-14 17:57:24 +0000

    Load Python requirements.txt

--- script/vagrant/setup_python.sh
+++ script/vagrant/setup_python.sh
@@ -5,6 +5,5 @@ export WORKING_DIR=/home/python
 virtualenv $WORKING_DIR
 chmod +x $WORKING_DIR/bin/*
 $WORKING_DIR/bin/activate
-$WORKING_DIR/bin/pip install spacy
-# pip install -r /vagrant/script/requirements.txt
+$WORKING_DIR/bin/pip install -r /vagrant/script/requirements.txt
 $WORKING_DIR/bin/python -m spacy.en.download
