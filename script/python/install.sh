1aa07eb99dafc676a8e968166f43c106f719c94d
m1cr0man <lucas@vccomputers.ie>
2016-03-29 16:31:48 +0100

    Fix permissions in python install script

--- script/python/install.sh
+++ script/python/install.sh
@@ -8,14 +8,17 @@
 export WORKING_DIR=${1:-"/home/python"}
 
 # If virtualenv fails, install it with apt get and try again
-virtualenv $WORKING_DIR || { apt-get -y update; apt-get -y install python-virtualenv; virtualenv $WORKING_DIR; }
+virtualenv -p python2 $WORKING_DIR || { apt-get -y update; apt-get -y install python-virtualenv; virtualenv -p python2 $WORKING_DIR; }
 
 source $WORKING_DIR/bin/activate
 
-pip install -r /vagrant/script/python/requirements.txt
-python -m spacy.en.download
+pip2 install -r /vagrant/script/python/requirements.txt
+python2 -m spacy.en.download
 
 # this ensures we know where the env is regardless of install location
 ln -s $WORKING_DIR /vagrant/script/python/env
 
 deactivate  # this is added to the path when activate is sourced
+
+chown -R vagrant:vagrant $WORKING_DIR
+chown vagrant:vagrant /vagrant/script/python/env
