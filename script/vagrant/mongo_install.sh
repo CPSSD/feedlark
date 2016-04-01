b892bac39cf20926b19664e01cf7656e004914d5
m1cr0man <lucas@vccomputers.ie>
2016-04-01 14:16:08 +0100

    Change & Remove mongo & gearman start commands

--- script/vagrant/mongo_install.sh
+++ script/vagrant/mongo_install.sh
@@ -7,8 +7,6 @@ chown -R mongodb:mongodb /home/vagrant/.mongodb
 sudo mv /etc/mongod.conf /etc/mongod.conf.orig
 sudo rm -f /etc/mongod.conf
 sudo cp /vagrant/script/vagrant/mongod.conf /etc/mongod.conf
-sudo systemctl enable mongod
-sudo systemctl start mongod
-sudo systemctl enable gearman-job-server
-sudo systemctl start gearman-job-server
+sudo service mongod start
+sudo service gearman-job-server start
 mongo /vagrant/script/vagrant/create_feed_user_db.js
