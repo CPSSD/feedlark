14a8cca27fb311bc71073f129cd706bf3455b78f
iandioch <iandioch11@gmail.com>
2016-04-01 20:28:00 +0100

    Add test cases and code for other 2 average functions

--- visualisations/all-topics/all_topics.py
+++ visualisations/all-topics/all_topics.py
@@ -3,4 +3,44 @@ import sys
 def mean(arr):
     if arr is None or len(arr) is 0:
         return None
-    return sum(arr) / len(arr)
+    return sum(arr) / (len(arr) + 0.0)
+
+def mode(arr):
+    #for performance reasons, this requires the input to be sorted in ascending order (which is required for median calculation anyway)
+    if arr is None or len(arr) == 0:
+        return None
+    curr_val = None
+    curr_count = 0
+    curr_mode = curr_val
+    mode_count = 0
+
+    for i in xrange(0, len(arr)):
+        if arr[i] < curr_val:
+            raise ValueError('Input is not sorted: ' + str(arr[i]) + ' < ' + str(curr_val))
+        if arr[i] is not curr_val: # we've reached a new number
+            if curr_count > mode_count:
+                 mode_count = curr_count
+                 curr_mode = curr_val
+            elif curr_count == mode_count:
+                 curr_mode = None
+            curr_count = 0
+            curr_val = arr[i]
+        curr_count += 1
+
+    if curr_count > mode_count:
+        curr_mode = curr_val
+    elif curr_count == mode_count:
+        curr_mode = None
+    return curr_mode
+
+def median(arr):
+    # requires input to be sorted, raises ValueError if it isn't.
+    for i in range(1, len(arr)):
+        if arr[i] < arr[i-1]:
+            raise ValueError('Input is not sorted: ' + str(arr[i]) + ' < ' + str(arr[i-1]))
+    if arr is None or len(arr) == 0:
+        return None
+    if len(arr) % 2 == 0:
+        return mean(arr[len(arr)/2-1:len(arr)/2+1])
+    else:
+        return arr[len(arr)/2]
--- visualisations/all-topics/testing.py
+++ visualisations/all-topics/testing.py
@@ -6,10 +6,32 @@ class TestScraping(unittest.TestCase):
 
     def test_mean(self):
         self.assertEqual(topics.mean([]), None)
+        self.assertEqual(topics.mean([1]), 1)
+        self.assertEqual(topics.mean([1,2]), 1.5)
         self.assertEqual(topics.mean([1,1,1,1,1]), 1)
         self.assertEqual(topics.mean([1,2,3,4,5]), 3)
         self.assertEqual(topics.mean([1.1,2.2,2.3,2.3,4.6,82.9]), 15.9)
         self.assertEqual(topics.mean([-5, -6, 12, -6, -4, 2097]), 348)
+
+    def test_mode(self):
+        self.assertEqual(topics.mode([]), None)
+        self.assertEqual(topics.mode([1,1,2,2]), None)
+        self.assertRaises(ValueError, topics.mode, [5,4,3,2,1]) # function assumes input is order in ascending order
+        self.assertRaises(ValueError, topics.mode, [1,2,2,2,2,4,3])
+        self.assertEqual(topics.mode([1,1,1,2,2]), 1)
+        self.assertEqual(topics.mode([1]), 1)
+        self.assertEqual(topics.mode([52,54,56,58]), None)
+
+    def test_median(self):
+        self.assertEqual(topics.median([]), None)
+        self.assertEqual(topics.median([1]), 1)
+        self.assertEqual(topics.median([1,1]), 1)
+        self.assertEqual(topics.median([1,2]), 1.5)
+        self.assertEqual(topics.median([1,2,3]), 2)
+        self.assertEqual(topics.median([1,2,2,3]), 2)
+        self.assertEqual(topics.median([1,2,3,4]), 2.5)
+        self.assertRaises(ValueError, topics.median, [5,4,3,2,1])
+        self.assertRaises(ValueError, topics.median, [1,2,2,2,2,4,3])
 
 if __name__ == '__main__':
     unittest.main()
