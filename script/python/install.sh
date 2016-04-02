0bc3709e56a85eea69d5e628e35ae36094c38087
Aaron Delaney <devoxel@gmail.com>
2016-04-02 17:25:37 +0100

    Add ignore installed flag to virtualenv

--- script/python/install.sh
+++ script/python/install.sh
@@ -5,6 +5,8 @@
 WORKING_DIR="/home/python"
 
 # If virtualenv fails, install it with apt get and try again
+# --system-site-packages lets us inherit numpy and such from the system python
+# install
 virtualenv -p --system-site-packages python2 $WORKING_DIR || {
   apt-get -y update;
   apt-get -y install python-virtualenv;
@@ -13,7 +15,7 @@ virtualenv -p --system-site-packages python2 $WORKING_DIR || {
 
 source $WORKING_DIR/bin/activate
 
-pip2 install -r /vagrant/script/python/requirements.txt
+pip2 install --ignore-installed -r /vagrant/script/python/requirements.txt
 python2 -m spacy.en.download
 
 deactivate  # this is added to the path when activate is sourced
