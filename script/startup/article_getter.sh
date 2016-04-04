b9a441455eb18c5f2632bdcf5bca256b0cbe026c
Aaron Delaney <devoxel@gmail.com>
2016-04-04 22:35:04 +0100

    Make startup script relative

--- script/startup/article_getter.sh
+++ script/startup/article_getter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+# should be run by start_internal, otherwise this relative path is wrong.
 source /home/python/bin/activate
-python /vagrant/article_getter/gman_art_getter.py
+python ../../article_getter/gman_art_getter.py
