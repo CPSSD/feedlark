ff3a45eb99724e9cae5f6faa701c4f0ede3d12e7
Aaron Delaney <devoxel@gmail.com>
2016-04-05 00:23:29 +0100

    Fix go install

--- script/vagrant/go_install.sh
+++ script/vagrant/go_install.sh
@@ -1,10 +1,16 @@
 #! /bin/sh
 
-echo "export GOPATH=/home/vagrant/.go" > /home/vagrant/.profile
-echo "export PATH=/vagrant/server/node_modules/.bin:$PATH:" >> /home/vagrant/.profile
-mkdir -p /home/vagrant/.go
-export GOPATH=/home/vagrant/.go
+if [ "$ENVIRONMENT" = "PRODUCTION" ]; then
+  echo "export GOPATH=/home/feedling/.go" > /home/vagrant/.profile
+  mkdir -p /home/feedling/.go
+  export GOPATH=/home/vagrant/.go
+else
+  echo "export GOPATH=/home/vagrant/.go" > /home/vagrant/.profile
+  echo "export PATH=/vagrant/server/node_modules/.bin:$PATH:" >> /home/vagrant/.profile
+  mkdir -p /home/vagrant/.go
+  export GOPATH=/home/vagrant/.go
+fi
+
 go get github.com/mikespook/gearman-go/worker
 go get gopkg.in/mgo.v2
 go get gopkg.in/mgo.v2/bson
-
