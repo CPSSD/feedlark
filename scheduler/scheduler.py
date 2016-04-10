3e41e9fee3c47f4027e7385da07fd7fcfd5049f3
Cian Ruane <cian.ruane1@gmail.com>
2016-04-10 21:26:43 +0100

    Add secret key support to scheduler

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,10 +1,21 @@
 import gearman
+import bson
 import time
+import os
 
 gm_client = gearman.GearmanClient(['localhost:4730'])
-while True:
-    gm_client.submit_job('update-all-feeds','')
 
-    #Once scraper is done aggregate
-    gm_client.submit_job('aggregate','')
+key = os.getenv('SECRETKEY')
+if key is not None:
+    request = bson.BSON.encode({
+        'key': key,
+        })
+else:
+    request = bson.BSON.encode({})
+
+while True:
+    gm_client.submit_job('update-all-feeds', str(request))
+
+    # Once scraper is done aggregate
+    gm_client.submit_job('aggregate', str(request))
     time.sleep(30)
