b1fc34b86ed6379f2cc5fabe5cec268665f57ce8
iandioch <iandioch11@gmail.com>
2016-04-11 23:38:25 +0100

    Implement basic classification

--- aggregator/predict.py
+++ aggregator/predict.py
@@ -1,27 +1,44 @@
 # The tool to predict how much interest a user will have in an article
 
+import pickle 
+import gearman
 from sklearn import linear_model
 
-class Regression:
+class Classification:
     model = None
 
     def __init__(self):
-        self.model = linear_model.SGDClassifier(loss="log") # loss="log" makes it use logistic regression
-
-    def train(self, x, y):
-        self.model.fit(x, y)
+        # loss="log" makes it use logistic regression
+        self.model = linear_model.SGDClassifier(loss="log")
 
     def predict(self, x):
-        probs = [a[0] for a in self.model.predict_proba(x)]
-        ans = self.model.predict(x)
-        print(probs)
-        print(ans)
+        """expects a single flat list, x, the inputs for the prediction.
+        outputs a single float, how likely it is the article will be in 'liked' category"""
+
+        print x
+
+        #this is a list of list, where the outer list is the data for each input,
+        #and the inner list is how likely that input will match each class
+        probabilities = self.model.predict_proba(x)
+
+        print probabilities
+
+        #get how likely each input will match class 0, ie. a 'like'
+        probabilities_of_like = [a[0] for a in probabilities]
         
+        print probabilities_of_like
+        return probabilities_of_like
+
+    def load_model(self, pickled_model):
+        self.model = pickle.loads(pickled_model) 
+        
+    def train(self, x, y):
+        self.model.partial_fit(x, y, classes=[0, 1])
 
 if __name__ == '__main__':
-    reg = Regression()
-    reg.train([[1, 2], [1, -1]], [0, 1])
-    reg.predict([[1,2]])
-    reg.predict([[2,-3]])
-    reg.predict([[1,-7]])
-    reg.predict([[1,0]])
+    c = Classification()
+    c.train([[1, 5], [2, -5], [2, 7], [1, -3], [1,0]], [0, 1, 0, 1, 0])
+    print c.predict([[1, 10]])
+    print c.predict([[2, 4]])
+    print c.predict([[3, -10]])
+    print c.predict([[-1, 11]])
