62c4dcad02d2a509337a0e5d136299e53dee8321
SailSlick <ross.osullivan37@mail.dcu.ie>
2016-04-15 13:28:30 +0100

    Initial Commit

---
+++ visualisations/popular_feeds/README.md
@@ -0,0 +1,37 @@
+Visualisation: Most Popular Feeds
+=================================
+
+This module will output statistics about the feeds that most users subscribe to.
+
+Dependencies
+------------
+
+- Python 2.7
+
+How to do tests
+------------
+
+The tests are written with the unittest module in python.
+
+Move to this directory, and run `python testing.py` to run the unit tests.
+
+To add unit tests modify the testing.py file.
+Check out the unittest docs for examples and general help.
+
+Usage
+-----
+
+Run `python popular_feeds.py X`, where X is the number of the most popular feeds you want the program to output data on. If this number is larger than the total number of feeds, the program will output data for every feed in the database.
+
+The first line of the output is the number of feeds in the system. This is followed the top X most popular feeds in descending order in the following format:
+
+`feed_name num_subscribers`
+
+```
+3
+https://news.ycombinator.com/rss 100
+http://spritesmods.com/rss.php 40
+http://dave.cheney.net/feed 10
+```
+
+All of this data is computed at run-time, and is not cached, so might take some time.
---
+++ visualisations/popular_feeds/popular_feeds.py
@@ -0,0 +1,58 @@
+import sys
+import gearman
+import bson
+import os
+
+def make_list_feeds(users):
+    feed_count = {}
+    for user in users:
+        for feed in user["subscribed_feeds"]:
+            if feed in feed_count:
+                feed_count[feed] += 1
+            else:
+                feed_count[feed] = 1
+    return feed_count
+
+def main():
+    if len(sys.argv) != 2:
+        print 'This tool takes 1 command line argument; the number of topics to output data on. See README.md'
+        return
+    num_requested_feeds = int(sys.argv[1])
+    gearman_client = gearman.GearmanClient(['localhost:4730'])
+    result = bson.BSON.decode(bson.BSON(gearman_client.submit_job('db-get', str(bson.BSON.encode({
+        'key': os.getenv('SECRETKEY'),
+        'database':'feedlark',
+        'collection':'user',
+        'query':{},
+        'projection':{
+            'subscribed_feeds':1
+        }
+    }))).result))
+
+    if result[u'status'] == u'ok':
+        # add the feeds to a dictionary as keys with count as variables
+        feed_counts = make_list_feeds(result["docs"])
+        sorted_feed = sorted(feed_counts,reverse=True)
+
+        #get ouput ready
+        output = ""
+        output += str(len(feed_counts)) + "\n"
+        
+        if num_requested_feeds > len(sorted_feed):
+            num_requested_feeds = len(sorted_feed)
+
+        for i in xrange(num_requested_feeds):
+            output += sorted_feed[i] + " " + str(feed_counts[sorted_feed[i]]) + "\n"
+
+        # output to file
+        f = open("most_popular_feeds.txt", "w")
+        f.write(output)
+        f.close()
+
+    else:
+        print 'Error getting user data from database'
+        print result['description']
+        return
+
+if __name__ == "__main__":
+    main()
