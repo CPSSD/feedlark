59f48cdfc977ac26661973c36c340efef6c8603c
Cian Ruane <cian.ruane1@gmail.com>
2016-04-16 23:20:10 +0100

    Add basic request functionality

--- cli/cli.py
+++ cli/cli.py
@@ -1,5 +1,21 @@
 import gearman
 import workers
+from bson import BSON
+
+
+def get_server():
+    print ''
+    serv = raw_input('Please enter Gearman server (or blank for localhost): ')
+    serv = serv if serv else 'localhost'
+
+    port = raw_input('Please enter Gearman port (or blank for 4730): ')
+    port = port if port else '4730'
+
+    return server + ':' + port
+
+
+def buid_request(work_class):
+    pass
 
 
 def main():
@@ -11,12 +27,20 @@ def main():
 #    # Turn that list into a dict with the worker names as keys
 #    w_dict = {x.NAME: x for x in w_list}
 
+    server = get_server()
+    try:
+        gm_client = gearman.GearmanClient([server])
+    except:
+        print "Error connecting to gearman server."
+        print "Is {} a valid server?".format(server)
+        return
+
     while True:
         print ''
         print "Commands available:"
 
         for i, worker in enumerate(w_list):
-            print '    ({}) {}'.format(i, worker.NAME)
+            print '    ({}) {}'.format(i, worker.NICENAME)
         print '    ({}) Exit'.format(i + 1)
 
         print ''
@@ -24,12 +48,31 @@ def main():
 
         try:
             c = int(c)
-            assert c <= len(w_list)
+            assert 0 <= c <= len(w_list)
         except:
             print 'Please provide an int between 0 and {}'.format(len(w_list))
             continue
 
-        print w_list[c]
+        if c == len(w_list):
+            print "See ya later, alligator"
+            return
+        else:
+            worker = w_list[c]
+
+        request = buid_request(worker.REQUEST)
+        bson_req = str(BSON.encode(request))
+
+        print ''
+        print "Submitting request to '{}'".format(worker.NAME)
+        bsond_response = gm_client.submit_job(worker.NAME, bson_req)
+        response = BSON(bsond_response).decode()
+
+        if worker.is_error(response):
+            print "There was an error with the request:"
+            print worker.get_error(response)
+            continue
+
+        worker.print_response(response)
 
 if __name__ == '__main__':
     main()
--- cli/workers.py
+++ cli/workers.py
@@ -1,13 +1,26 @@
 class WorkerSkeleton:
+    @staticmethod
     def is_error(response):
         return False
 
+    @staticmethod
     def get_error(response):
         return 'No description provided'
+
+    @classmethod
+    def print_dict(cls, response, indent=0):
+        for key in response:
+            if type(response[key]) == dict:
+                print ' '*indent + str(key) + ': '
+                cls.print_dict(response[key], 4)
+                continue
+
+            print ' '*indent + str(key) + ': ' + str(response[key])
 
 
 class dbgetWorker(WorkerSkeleton):
     NAME = 'db-get'
+    NICENAME = 'Database Get'
     REQUEST = {
         'key': str,
         'database': str,
@@ -16,21 +29,26 @@ class dbgetWorker(WorkerSkeleton):
         'projection': dict,
     }
 
+    @staticmethod
     def is_error(response):
         return 'status' not in response or response['status'] == 'error'
 
+    @staticmethod
     def get_error(response):
         return response['description']
 
 
 class AggregatorWorker(WorkerSkeleton):
     NAME = 'aggregate'
+    NICENAME = 'Aggregate'
     REQUEST = {
         'key': str,
     }
 
+    @staticmethod
     def is_error(response):
         return 'status' not in response or response['status'] == 'error'
 
+    @staticmethod
     def get_error(response):
         return response['description']
