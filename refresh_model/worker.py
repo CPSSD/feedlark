c15ce7b089f9fdf00d25ade2b89c943835b7cc9c
iandioch <iandioch11@gmail.com>
2016-04-16 22:06:23 +0100

    Add database getting functions

--- refresh_model/worker.py
+++ refresh_model/worker.py
@@ -7,7 +7,47 @@ key = getenv('SECRETKEY')
 def log(level, message):
     levels = ['INFO:', 'WARNING:', 'ERROR:']
     time = datetime.now().strftime('%H:%M %d/%m/%Y')
-    print ' '.join([time, levels[level], message)
+    print ' '.join([time, levels[level], message])
+
+def get_user_data(username):
+    req_data = bson.BSON.encode({
+        "key": key,
+        "database": "feedlark",
+        "collection": "user",
+        "query": {
+            "username": username
+        },
+        "projection": {}
+    })
+    get_response = gearman_client.submit_job('db-get', str(req_data))
+    result = bson.BSON(get_response.result).decode()
+    if result['status'] != 'ok':
+        log(2, "Error getting db entry for user {}".format(username))
+        return None
+    if "docs" not in result or len(result['docs']) == 0:
+        log(1, "No docs returned for user {}".format(username))
+        return None
+    return result['docs'][0]
+
+def get_votes_for_user(username):
+    req_data = bson.BSON.encode({
+        "key": key,
+        "database": "feedlark",
+        "collection": "vote",
+        "query": {
+            "username": username
+        },
+        "projection": {}
+    })
+    get_response = gearman_client.submit_job('db-get', str(req_data))
+    result = bson.BSON(get_response).decode()
+    if result['status'] != 'ok':
+        log(2, "Error getting votes for user {}".format(username))
+        return
+    if 'docs' not in result:
+        log(1, "No docs returned for user {}".format(username))
+        return None
+    return result['docs']
 
 def refresh_model(worker, job):
     """
