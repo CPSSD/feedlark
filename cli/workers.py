3b8ef92d209d2482fb6029b69aabda4e8afca4e0
Cian Ruane <cian.ruane1@gmail.com>
2016-04-17 01:17:22 +0100

    Move decorators to new file and remove TemplateWorker

--- cli/workers.py
+++ cli/workers.py
@@ -1,41 +1,4 @@
-def standard_error(in_class):
-    @staticmethod
-    def is_error(response):
-        return 'status' not in response or response['status'] == 'error'
-
-    @staticmethod
-    def get_error(response):
-        return response['description']
-
-    in_class.is_error = is_error
-    in_class.get_error = get_error
-    return in_class
-
-
-def standard_response(in_class):
-    @classmethod
-    def print_response(cls, response, indent=4):
-        for key in response:
-            if type(response[key]) == dict:
-                print ' '*indent + str(key) + ': '
-                cls.print_dict(response[key], indent + 4)
-                continue
-
-            print ' '*indent + str(key) + ': ' + str(response[key])
-
-    in_class.print_response = print_response
-    return in_class
-
-
-@standard_response
-@standard_error
-class TemplateWorker:
-    NAME = 'gm-worker-name'
-    NICENAME = 'Name For Menu'
-    REQUEST = {
-        'key': str,
-        'url': str,
-    }
+from decorators import standard_error, standard_response
 
 
 @standard_response
