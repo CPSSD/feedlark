ae355cf0d1c14b35eebd5781c8427087df130099
iandioch <iandioch11@gmail.com>
2016-04-18 18:58:34 +0100

    Modify refresh_model test, add logging

--- refresh_model/testing.py
+++ refresh_model/testing.py
@@ -4,13 +4,15 @@ from datetime import datetime
 
 class TestRefreshModel(unittest.TestCase):
     def test_get_votes(self):
+        print 'Testing refresh-model get_votes_for_user()'
         init_gearman_client()
         votes = get_votes_for_user('iandioch')
-        #self.assertTrue(len(votes) > 0)
+        self.assertTrue(len(votes) > 0)
         votes = get_votes_for_user(6969)
         self.assertFalse(len(votes) > 0)
 
     def test_get_user_data(self):
+        print 'Testing refresh-model get_user_data'
         init_gearman_client()
         user_data = get_user_data('iandioch')
         self.assertTrue(user_data is not None)
@@ -18,6 +20,7 @@ class TestRefreshModel(unittest.TestCase):
         self.assertTrue(user_data is None)
 
     def test_update_model(self):
+        print 'Testing refresh-model build_model'
         init_gearman_client()
         user_data = get_user_data('iandioch')
         if 'model' in user_data:
@@ -28,10 +31,12 @@ class TestRefreshModel(unittest.TestCase):
         user_data = get_user_data('iandioch')
         self.assertFalse('model' in user_data)
 
+        print 'Building empty model'
         votes = []
         model = build_model(user_data, votes)
         self.assertTrue(model is None)
 
+        print 'Building populated model'
         votes = [{
             'username': 'iandioch',
             'article_url': 'http://www.apple.com/customer-letter/',
@@ -41,12 +46,6 @@ class TestRefreshModel(unittest.TestCase):
         }]
         model = build_model(user_data, votes)
         self.assertFalse(model is None)
-
-        user_data['model'] = model
-        update_user_data('iandioch', user_data)
-
-        user_data = get_user_data('iandioch')
-        self.assertTrue('model' in user_data)
 
 if __name__ == '__main__':
     unittest.main()
