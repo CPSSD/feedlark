56bd373e5286bfd364b6b73bca372715de5f996f
iandioch <iandioch11@gmail.com>
2016-04-25 18:01:24 +0100

    Remove hanging test

--- refresh_model/testing.py
+++ refresh_model/testing.py
@@ -19,33 +19,5 @@ class TestRefreshModel(unittest.TestCase):
         user_data = get_user_data(1337)
         self.assertTrue(user_data is None)
 
-    def test_update_model(self):
-        print 'Testing refresh-model build_model'
-        init_gearman_client()
-        user_data = get_user_data('iandioch')
-        if 'model' in user_data:
-            del user_data['model']
-
-        update_user_data('iandioch', user_data)
-        
-        user_data = get_user_data('iandioch')
-        self.assertFalse('model' in user_data)
-
-        print 'Building empty model'
-        votes = []
-        model = build_model(user_data, votes)
-        self.assertTrue(model is None)
-
-        print 'Building populated model'
-        votes = [{
-            'username': 'iandioch',
-            'article_url': 'http://www.apple.com/customer-letter/',
-            'feed_url': 'https://news.ycombinator.com/rss',
-            'positive_opinion': False,
-            'vote_datetime': datetime.now()
-        }]
-        model = build_model(user_data, votes)
-        self.assertFalse(model is None)
-
 if __name__ == '__main__':
     unittest.main()
