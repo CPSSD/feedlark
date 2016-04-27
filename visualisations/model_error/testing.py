001de572a1748333ef8ad4df0cce1583059a8438
iandioch <iandioch11@gmail.com>
2016-04-27 19:06:39 +0100

    Add tests for new functions

--- visualisations/model_error/testing.py
+++ visualisations/model_error/testing.py
@@ -7,6 +7,18 @@ class TestDbTools(unittest.TestCase):
     def test_get_username(self):
         self.assertEqual(me.get_username_from_input(['', 'jeremy']), 'jeremy')
         self.assertEqual(me.get_username_from_input(['', 'jeremy', 'corbyn']), 'jeremy corbyn')
+    
+    def test_has_enough_classes(self):
+        data = [[0, 1], [0, 1], [0, 1]]
+        self.assertFalse(me.has_enough_classes(data))
+        data.append([0, -1])
+        self.assertTrue(me.has_enough_classes(data))
+    
+    def test_get_model_score(self):
+        training = [[[1, -1], 1], [[-1, 1], -1]]
+        validation = [[[1, -1], 1]]
+
+        self.assertTrue(me.get_model_score(training, validation) > 0.5)
 
 if __name__ == '__main__':
     unittest.main()
